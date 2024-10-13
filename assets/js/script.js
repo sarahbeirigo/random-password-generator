4e8872921b42a87121ff34d2742cea8d1aa3c2f0
Sarah <sarahcbeirigo@gmail.com>
2024-10-13 20:27:06 -0300

    subindo projeto

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const characters = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+var rangerSlider = document.getElementById('slider-range');
+const h1 = document.getElementById('h1');
+let sliderValue = rangerSlider.value;
+
+rangerSlider.addEventListener('input', function () {
+    sliderValue = this.value;
+    passwordGenerate(sliderValue, characters);
+    
+    const passwordLenght = document.getElementById('password-lenght')
+    passwordLenght.textContent = sliderValue;
+    rangerSlider.parentNode.insertBefore(passwordLenght, rangerSlider);
+});
+
+
+function passwordGenerate(passwordSize, charactersArray) {
+    let password = new Array(passwordSize);
+
+    // console.log(caracteres.length)
+    for (let i = 0; i < passwordSize; i++) {
+        var x = Math.floor(Math.random() * charactersArray.length);
+        password[i] = charactersArray[x];
+    }
+
+    const passwordResult = document.getElementById('password-result');
+    passwordResult.textContent = password.join('');
+    h1.insertAdjacentElement('afterend', passwordResult);;
+
+}
+
+function copy() {
+    const textCopy = document.getElementById("password-result").innerText;
+    navigator.clipboard.writeText(textCopy);
+    window.alert("Copy!");
+}
+
+
