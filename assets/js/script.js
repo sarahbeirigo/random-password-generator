d52199aa99adab17607524a0d6027caeead223fc
Sarah <sarahcbeirigo@gmail.com>
2024-10-15 22:10:09 -0300

    feat: adicionar notificacao de copiado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 const characters = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 var rangerSlider = document.getElementById('slider-range');
 const h1 = document.getElementById('h1');
+
 let sliderValue = rangerSlider.value;
 
 rangerSlider.addEventListener('input', function () {
     sliderValue = this.value;
     passwordGenerate(sliderValue, characters);
-    
+
     const passwordLenght = document.getElementById('password-lenght')
     passwordLenght.textContent = sliderValue;
     rangerSlider.parentNode.insertBefore(passwordLenght, rangerSlider);
@@ -28,10 +29,20 @@ function passwordGenerate(passwordSize, charactersArray) {
 
 }
 
+
+const copyNot = document.getElementById('copy-not');
 function copy() {
     const textCopy = document.getElementById("password-result").innerText;
     navigator.clipboard.writeText(textCopy);
-    window.alert("Copy!");
+    copyNot.style.display = "block";
+
+    setTimeout(() => {
+        copyNot.style.display = "none";
+    }, 1500);
+
 }
+
+
+
 
 
